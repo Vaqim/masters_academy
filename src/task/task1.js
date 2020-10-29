ef6ea22250a3c017affd396b02c89d9d7c6d527c
Vaqim <vadimser1355@gmail.com>
2020-10-29 11:42:51 +0200

    test deleted

--- src/task/task1.js
+++ src/task/task1.js
@@ -1,10 +1,5 @@
-const arr = require('../../input_array');
-
 function firstTask(inputArray, param, value) {
   return inputArray.filter((e) => e[param] === value);
 }
-
-console.log(arr);
-console.log(firstTask(arr, 'color', 'red'));
 
 module.exports = firstTask;
