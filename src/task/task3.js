68f87b64ff0894ff324abf626183dd6dd3a65602
Vaqim <vadimser1355@gmail.com>
2020-10-29 12:37:19 +0200

    test deleted & some refactor

--- src/task/task3.js
+++ src/task/task3.js
@@ -1,1 +1,12 @@
- 
+function thirdTask(inputArray) {
+  return inputArray.map((e) => {
+    if (!e.quantity) e.quantity = 0;
+    if (e.priceForPair) {
+      e.price = e.priceForPair;
+      delete e.priceForPair;
+    }
+    return e;
+  });
+}
+
+module.exports = thirdTask;
