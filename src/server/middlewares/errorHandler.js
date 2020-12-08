0dbbb3901453e91c9c9c4a28050f3b18642be666
Vaqim <vadimser1355@gmail.com>
2020-12-08 22:26:22 +0200

    unlog auth errors

--- src/server/middlewares/errorHandler.js
+++ src/server/middlewares/errorHandler.js
@@ -1,10 +1,10 @@
 function errorHandler(err, req, res, next) {
-  console.log(err);
   switch (err.name) {
     case 'AuthorizationError':
       res.status(403).send(err.message);
       break;
     default:
+      console.log(err);
       res.status(500).send(err.message);
       break;
   }
