d203c443c44b8c90ed5bed26d6063df388ccc6d7
Vaqim <vadimser1355@gmail.com>
2020-12-25 13:08:21 +0200

    seeds rebuild

--- src/db/seeds/index.js
+++ src/db/seeds/index.js
@@ -3,22 +3,14 @@ const { createColor, createType, close, testConnection } = require('../index');
 const colors = ['red', 'black', 'lime', 'navy', 'purple'];
 const types = ['socks', 'gloves', 'hat', 'jeans'];
 
-const seeds = async () => {
-  try {
-    await testConnection();
+testConnection();
 
-    colors.forEach(async (e) => {
-      await createColor(e);
-    });
+colors.forEach(async (e) => {
+  await createColor(e);
+});
 
-    types.forEach(async (e) => {
-      await createType(e);
-    });
+types.forEach(async (e) => {
+  await createType(e);
+});
 
-    close();
-  } catch (error) {
-    console.error('Seeds Error: ', error.message || error);
-  }
-};
-
-seeds();
+close();
