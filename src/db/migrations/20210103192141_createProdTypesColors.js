b43a99bc53b17ad6c300688bca9526bf35b2f3cf
Vaqim <vadimser1355@gmail.com>
2021-01-07 12:39:23 +0200

    fix created_at timestamp

--- src/db/migrations/20210103192141_createProdTypesColors.js
+++ src/db/migrations/20210103192141_createProdTypesColors.js
@@ -2,19 +2,6 @@ exports.up = async (knex) => {
   await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 
   await knex.schema.raw(`
-  CREATE OR REPLACE FUNCTION created_at_timastamp() RETURNS TRIGGER
-    LANGUAGE plpgsql
-    AS
-    $$
-    BEGIN
-      NEW.created_at = CURRENT_TIMESTAMP;
-      NEW.updated_at = CURRENT_TIMESTAMP;
-      RETURN NEW;
-    END;
-    $$;
-  `);
-
-  await knex.schema.raw(`
   CREATE OR REPLACE FUNCTION updated_at_timastamp() RETURNS TRIGGER
     LANGUAGE plpgsql
     AS
@@ -45,20 +32,12 @@ exports.up = async (knex) => {
     table.decimal('price').notNullable();
     table.integer('quantity').notNullable().unsigned();
     table.timestamp('deleted_at').nullable();
-    table.timestamps();
+    table.timestamps(true, true);
   });
 
   await knex.schema.raw(
     'ALTER TABLE products ADD CONSTRAINT uniq_product UNIQUE(color_id, type_id, price)',
   );
-
-  await knex.schema.raw(`
-  CREATE TRIGGER created_at_timastamp
-  BEFORE INSERT
-  ON products
-  FOR EACH ROW
-  EXECUTE PROCEDURE created_at_timastamp();
-  `);
 
   await knex.schema.raw(`
   CREATE TRIGGER updated_at_timastamp
@@ -74,7 +53,6 @@ exports.down = async (knex) => {
   await knex.schema.dropTable('products');
   await knex.schema.dropTable('types');
   await knex.schema.dropTable('colors');
-  await knex.schema.raw('DROP FUNCTION IF EXISTS created_at_timastamp() CASCADE');
   await knex.schema.raw('DROP FUNCTION IF EXISTS updated_at_timastamp() CASCADE');
   await knex.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp" CASCADE');
 };
