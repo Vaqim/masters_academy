6665b10d3c1d7eca3f9d38c7c37d0f6ac06ad636
Vaqim <vadimser1355@gmail.com>
2021-01-22 13:33:03 +0200

    migration file update

--- src/db/migrations/20210103192141_createProdTypesColors.js
+++ src/db/migrations/20210103192141_createProdTypesColors.js
@@ -1,29 +1,28 @@
 exports.up = async (knex) => {
-  await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
-
-  await knex.schema.raw(`
-  CREATE OR REPLACE FUNCTION updated_at_timastamp() RETURNS TRIGGER
-    LANGUAGE plpgsql
-    AS
-    $$
-    BEGIN
-      NEW.updated_at = CURRENT_TIMESTAMP;
-      RETURN NEW;
-    END;
-    $$;
-  `);
-
-  await knex.schema.createTable('colors', (table) => {
-    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name').unique().notNullable();
-    table.timestamp('deleted_at').nullable();
-  });
-
-  await knex.schema.createTable('types', (table) => {
-    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
-    table.string('name').unique().notNullable();
-    table.timestamp('deleted_at').nullable();
-  });
+  await Promise.all([
+    knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"'),
+    knex.schema.raw(`
+      CREATE OR REPLACE FUNCTION updated_at_timastamp() RETURNS TRIGGER
+        LANGUAGE plpgsql
+        AS
+        $$
+        BEGIN
+          NEW.updated_at = CURRENT_TIMESTAMP;
+          RETURN NEW;
+        END;
+        $$;
+    `),
+    knex.schema.createTable('colors', (table) => {
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+      table.string('name').unique().notNullable();
+      table.timestamp('deleted_at').nullable();
+    }),
+    knex.schema.createTable('types', (table) => {
+      table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
+      table.string('name').unique().notNullable();
+      table.timestamp('deleted_at').nullable();
+    }),
+  ]);
 
   await knex.schema.createTable('products', (table) => {
     table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary();
@@ -33,11 +32,8 @@ exports.up = async (knex) => {
     table.integer('quantity').notNullable().unsigned();
     table.timestamp('deleted_at').nullable();
     table.timestamps(true, true);
+    table.unique(['color_id', 'type_id', 'price'], 'uniq_product');
   });
-
-  await knex.schema.raw(
-    'ALTER TABLE products ADD CONSTRAINT uniq_product UNIQUE(color_id, type_id, price)',
-  );
 
   await knex.schema.raw(`
   CREATE TRIGGER updated_at_timastamp
@@ -49,10 +45,13 @@ exports.up = async (knex) => {
 };
 
 exports.down = async (knex) => {
-  await knex.schema.raw('ALTER TABLE products DROP CONSTRAINT uniq_product');
   await knex.schema.dropTable('products');
-  await knex.schema.dropTable('types');
-  await knex.schema.dropTable('colors');
-  await knex.schema.raw('DROP FUNCTION IF EXISTS updated_at_timastamp() CASCADE');
+
+  await Promise.all([
+    knex.schema.dropTable('types'),
+    knex.schema.dropTable('colors'),
+    knex.schema.raw('DROP FUNCTION IF EXISTS updated_at_timastamp() CASCADE'),
+  ]);
+
   await knex.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp" CASCADE');
 };
